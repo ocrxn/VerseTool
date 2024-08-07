0bbca655f17788f9cb1a3c8e95178857b29df895
ocrxn <philip.maly@outlook.com>
2024-08-06 20:36:55 -0400

    initial commit

---
+++ VerseTool.py
@@ -0,0 +1,149 @@
+verses = ["1. Joshua 1:9 Have not I commanded thee? Be strong and of a good courage; be not afraid, neither be thou dismayed: for the Lord thy God is with thee withersoever thou goest.",
+          "2. Nehemiah 4:6 So built we the wall; and all the wall was joined together unto the half thereof: for the people had a mind to work.",
+          "3. Psalms 19:14 Let the words of my mouth, and the meditation of my heart, be acceptable in thy sight, O Lord, my strength, and my redeemer.",
+          "4. Psalms 37:4 Delight thyself also in the Lord; and he shall give thee the desires of thine heart.",
+          "5. Psalms 40:7-8 Then said I, Lo, I come: in the volume of the book it is written of me, I delight to do thy will O my God: yea, thy law is within my heart.",
+          "6. Proverbs 13:4 The soul of the sluggard desireth, and hath nothing: but the soul of the diligent shall be made fat.",
+          "7. Isaiah 40:31 But they that wait upon the Lord shall renew their strength; they shall mount up with wings as eagles; they shall run, and not be weary; and they shall walk, and not faint.",
+          "8. Isaiah 41:10 Fear thou not; for I am with thee: be not dismayed; for I am thy God: I will strengthen thee; yea, I will help thee; yea, I will uphold thee with the right hand of my righteousness.",
+          "9. Jeremiah 33:3 Call unto me, and I will answer thee, and show thee great and mighty things, which thou knowest not.",
+          "10. Matthew 6:33 But seek ye first the kingdom of God, and his righteousness; and all these things shall be added unto you.",
+          "11. Matthew 9:38 Pray ye therefore the Lord of the harvest, that he will send forth labourers into his harvest.",
+          "12. Matthew 11:28-30 Come unto me, all ye that labour and are heavy laden, and I will give you rest. Take my yoke upon you and learn of me; for I am meek and lowly in heart: and ye shall find rest unto your souls. For my yoke is easy, and my burden is light.",
+          "13. John 3:14-16 And as Moses lifted up the serpent in the wilderness, even so must the Son of man be lifted up: That whosoever believeth in him should not perish, but have eternal life. For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.",
+          "14. John 3:36 He that believeth on the Son hath everlasting life: and he that believeth not the Son shall not see life; but the wrath of God abideth on him.",
+          "15. John 15:5 I am the vine, ye are the branches: He that abideth in me, and I in him, the same bringeth forth much fruit: for without me ye can do nothing.",
+          "16. Acts 1:8 But ye shall receive power, after that the Holy Ghost is come upon you: and ye shall be witnesses unto me both in Jerusalem, and in all Judea, and in Samaria, and unto the uttermost part of the earth.",
+          "17. Acts 4:12 Neither is there salvation in any other: for there is none other name under heaven given among men, whereby we must be saved.",
+          "18. Romans 1:16 For I am not ashamed of the gospel of Christ: for it is the power of God unto salvation to everyone that believeth; to the Jew first, and also to the Greek.",
+          "19. Romans 5:19 For as by one man's disobedience many were made sinners, so by the obedience of one shall many be made righteous.",
+          "20. Romans 8:18 For I reckon that the sufferings of this present time are not worthy to be compared with the glory which shall be revealed in us.",
+          "21. Romans 8:28 And we know that all things work together for good to them that love God, to them who are the called according to his purpose.",
+          "22. Romans 8:38-39 For I am persuaded, that neither death, nor life, nor angels, nor principalities, nor powers, nor things present, nor things to come, nor height, nor depth, nor any other creature, shall be able to separate us from the love of God, which is in Christ Jesus our Lord.",
+          "23. Romans 10:17 So then faith cometh by hearing, and hearing by the Word of God.",
+          "24. Romans 12:1 I beseech you therefore, brethren by the mercies of God, that ye present your bodies a living sacrifice, holy, acceptable unto God, which is your reasonable service.",
+          "25. 1 Corinthians 10:13 There hath no temptation taken you but such as is common to man: but God is faithful, who will not suffer you to be tempted above that ye are able; but will with the temptation also make a way to escape, that ye may be able to bear it.",
+          "26. 1 Corinthians 13:2 And though I have the gift of prophecy, and understand all mysteries, and all knowledge; and though I have all faith, so that I could remove mountains, and have not charity, I am nothing.",
+          "27. 2 Corinthians 6:14 Be ye not unequally yoked together with unbelievers: for what fellowship hath righteousness with unrighteousness? and what communion hath light with darkness?",
+          "28. Galatians 2:20 I am crucified with Christ: nevertheless I live; yet not I, but Christ liveth in me: and the life which I now live in the flesh I live by the faith of the Son of God, who loved me, and gave himself for me.",
+          "29. Ephesians 2:8-9 For by grace are ye saved through faith; and that not of yourselves: it is the gift of God: Not of works, lest any man should boast.",
+          "30. Philippians 1:6 Being confident of this very thing, that he which hath begun a good work in you will perform it until the day of Jesus Christ.",
+          "31. Philippians 4:13 I can do all things through Christ which strengtheneth me.",
+          "32. 1 Timothy 4:12 Let no man despise thy youth; but be thou an example of the believers, in word, in conversation, in charity, in spirit, in faith, in purity.",
+          "33. Hebrews 11:6 But without faith it is impossible to please him: for he that cometh to God must believe that he is, and that he is a rewarder of those that diligently seek him.",
+          "34. James 1:22 But be ye doers of the word, and not hearers only, deceiving your own selves.",
+          "35. James 4:7 Submit yourselves therefore to God. Resist the devil, and he will flee from you.",
+          "36. James 5:16 Confess your faults one to another, and pray one for another, that ye may be healed. The effectual fervent prayer of a righteous man availeth much.",
+          "37. 2 Peter 2:9 The Lord knoweth how to deliver the godly out of temptations, and to reserve the unjust unto the day of judgement to be punished:",
+          "38. 1 John 1:9 If we confess our sins, he is faithful and just to forgive us our sins, and to cleanse us from all unrighteousness.",
+          "39. 1 John 2:15 Love not the world, neither the things that are in the world. If any man love the world, the love of the Father is not in him.",
+          "40. 1 John 4:7-8 Beloved, let us love one another, for love is of God; and every one that loveth is born of God, and knoweth God. He that loveth not knoweth not God; for God is love."]
+keywords = ["commanded","strong","built","wall","joined","half","people","mind","words","mouth","meditation","sight","redeemer","said","book","written","law","soul","sluggard","diligent","wait","renew","mount","wings","eagles","run","weary","walk","faint","strengthen","help","uphold","right","hand","call","answer","show","great","kingdom","these","harvest","send","labourers","into","labour","rest","take","yoke","learn","find","souls","easy","burden","Moses","lifted","serpent","wilderness","perish","see","wrath","vine","branches","fruit","witnesses","Jerusalem","Judea","Samaria","uttermost","earth","name","heaven","among","men","ashamed","Jew","Greek","man's","disobedience","many","sinners","obedience","sufferings","time","worthy","compared","glory","revealed","called","according","purpose","persuaded","death","angels","principalities","powers","depth","separate","hearing","mercies","bodies","living","sacrifice","service","taken","common","tempted","above","make","escape","prophecy","understand","remove","mountains","mysteries","unequally","yoked","unbelievers","fellowship","darkness","crucified","now","flesh","live","himself","grace","works","boast","being","confident","very","begun","until","despise","youth","example","purity","impossible","rewarder","doers","hearers","own","selves","resist","devil","flee","healed","prayer","how","deliver","godly","sins","forgive","cleanse","beloved","born"]
+references = ["Joshua 1:9","Nehemiah 4:6","Psalms 19:14","Psalms 37:4","Psalms 40:7-8","Proverbs 13:4","Isaiah 40:31","Isaiah 41:10","Jeremiah 33:3","Matthew 6:33","Matthew 9:38","Matthew 11:28-30","John 3:14-16","John 3:36","John 15:5","Acts 1:8","Acts 4:12","Romans 1:16","Romans 5:19","Romans 8:18","Romans 8:28","Romans 8:38-39","Romans 10:17","Romans 12:1","1 Corinthians 10:13","1 Corinthians 13:2","2 Corinthians 6:14","Galatians 2:20","Ephesians 2:8-9","Philippians 1:6","Philippian 4:13","1 Timothy 4:12","Hebrews 11:6","James 1:22","James 4:7","James 5:16","2 Peter 2:9","1 John 1:9","1 John 2:15","1 John 4:7-8"]
+first_words = ["Have","So","Let","Delight","Then","The","But","Fear","Call","But","Pray","Come","And","He","I","But","Neither","For","For","For","And","For","So","I","There","And","Be","I","For","Being","I","Let","But","But","Submit","Confess","The","If","Love","Beloved"]
+last_words = ["goest","work","redeemer","heart","heart","fat","faint","righteousness","not","you","harvest","light","life","him","nothing","earth","saved","Greek","righteous","us","purpose","Lord","God","service","it","nothing","darkness","me","boast","Christ","me","purity","him","selves","you","much","punished","unrighteousness","him","love"]
+class VerseTool():
+    def __init__(self, verses) -> None:
+        self.verses = verses
+
+    def show_options(self):
+        print('''1. Search by number
+2. Search by reference
+3. Search by keyword
+4. Search by beginning of the verse
+5. Search by end of the verse
+''')
+
+    def display_by_number(self):#done
+        while True:
+            number_input = int(input("Enter number of verse to lookup('0' for main menu): "))
+            if number_input > 0 and number_input <= 40:
+                print(f"{verses[number_input - 1]}")
+            elif number_input == 0:
+                vTool.main()
+            else:
+                print("Number out of range.")
+
+    def display_by_reference(self):#done
+        while True:
+            reference_input = input("Enter reference('exit' for main menu): ")
+            if reference_input in references:
+                ref_index = [reference for reference in verses if reference_input in reference]
+                for i in range(len(ref_index)):
+                    rf_x = ref_index[i]
+                    print(rf_x)
+            elif reference_input == 'exit':
+                vTool.main()
+            else:
+                print("Error. Reference not found. Try checking your spelling.")
+
+    def display_by_keyword(self):#done
+        while True:
+            keyword_input = input("Enter keyword('exit' for main menu): ")
+            if keyword_input in keywords:
+                keyword_index = [keyword for keyword in verses if keyword_input in keyword]
+                for i in range(len(keyword_index)):
+                    kw_x = keyword_index[i]
+                    print(kw_x)
+            elif keyword_input == 'exit':
+                vTool.main()
+            else:
+                print("Error. Keyword not found. Try checking your spelling.")
+
+    def display_by_beginning(self):#
+        while True:
+            first_word_input = input("Enter first word('exit' for main menu): ")
+            if first_word_input in first_words:
+                first_word_ref_list = []
+                for i in range(len(first_words)):
+                    if first_words[i] == first_word_input:
+                        first_word_ref_list.append(verses[i])
+                for f_k in range(len(first_word_ref_list)):
+                    f_x = first_word_ref_list[f_k]
+                    print(f_x)
+
+            elif first_word_input == 'exit':
+                vTool.main()
+            else:
+                print("Error. First word not found. Try checking your spelling.")
+
+    def display_by_end(self):#
+        while True:
+            last_word_input = input("Enter last word('exit' for main menu): ")
+            if last_word_input in last_words:
+                last_word_ref_list = []
+                for i in range(len(last_words)):
+                    if last_words[i] == last_word_input:
+                        last_word_ref_list.append(verses[i])
+                for e_k in range(len(last_word_ref_list)):
+                    e_x = last_word_ref_list[e_k]
+                    print(e_x)
+            elif last_word_input == 'exit':
+                vTool.main()
+            else:
+                print("Error. First word not found. Try checking your spelling.")
+
+    def main(self):
+        vTool.show_options()
+        user_input = int(input("Enter number of choice (#1-5 or '0' to terminate): "))
+        if user_input == 1:
+            vTool.display_by_number()
+        elif user_input == 2:
+            vTool.display_by_reference()
+        elif user_input == 3:
+            vTool.display_by_keyword()   
+        elif user_input == 4:
+            vTool.display_by_beginning()
+        elif user_input == 5:
+            vTool.display_by_end()
+        elif user_input == 0:
+            print("Exiting program...")
+            exit()
+        else:
+            print("Invalid number.")
+if __name__ == '__main__':
+    vTool = VerseTool(verses)
+    vTool.main()
+            
+
+
